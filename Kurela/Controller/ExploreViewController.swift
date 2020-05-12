993afb8f9db6a4531a7583b3886be9bb2752d622
Habib Akbar <habibakbar360@gmail.com>
2020-05-12 20:17:49 +0700

    Initial Commit

---
+++ Kurela/Controller/ExploreViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ExploreViewController.swift
+//  Kurela
+//
+//  Created by M Habib Ali Akbar on 08/05/20.
+//  Copyright © 2020 Group 16 MC2 2020. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var cards: [Card] = [
+        Card(infoName: "Judul Berita 1", location: "Jakarta"),
+        Card(infoName: "Judul Berita 1", location: "Jakarta"),
+        Card(infoName: "Judul Berita 1", location: "Jakarta"),
+        Card(infoName: "Judul Berita 1", location: "Jakarta"),
+        Card(infoName: "Judul Berita 1", location: "Jakarta"),
+        Card(infoName: "Judul Berita 1", location: "Jakarta")
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.register(UINib(nibName: "CardCell", bundle: nil), forCellReuseIdentifier: "ReusableCell")
+    }
+    
+}
+
+extension ExploreViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("3")
+    }
+}
+
+extension ExploreViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cards.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! CardCell
+        
+        return cell
+    }
+    
+    
+    
+}
---
+++ Kurela/Model/Card.swift
@@ -0,0 +1,14 @@
+//
+//  Card.swift
+//  Kurela
+//
+//  Created by M Habib Ali Akbar on 08/05/20.
+//  Copyright © 2020 Group 16 MC2 2020. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    let infoName: String
+    let location: String
+}
