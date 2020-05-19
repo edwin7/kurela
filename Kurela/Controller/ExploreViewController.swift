52a84235723562cad1109966edb188bcbaf6d6e9
Edwin Sendjaja <edwin.sendjaja@gmail.com>
2020-05-19 17:06:23 +0700

    merged develop

--- Kurela/Controller/ExploreViewController.swift
+++ Kurela/Controller/ExploreViewController.swift
@@ -8,11 +8,105 @@
 
 import UIKit
 
-class ExploreViewController: UIViewController {
+
+class ExploreViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchControllerDelegate, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+   
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
+    @IBOutlet weak var cardTableView: UITableView!
+    
+    //content in card table view
+    let pictures: [UIImage] = [UIImage(named: "card1.png")!, UIImage(named: "card2.png")!, UIImage(named: "card3.png")!, UIImage(named: "card4.png")!, UIImage(named: "card5.png")!, UIImage(named: "card6.png")!,]
+    let titles: [String] = ["Relawan Siaga Erupsi Gunung Merapi", "Kado untuk Anak Jalanan Sekitar Kita", "Berbagi Sembako untuk Driver Online", "Donor Darah untuk Pasien COVID-19", "Sosialisasi Huruf Braille untuk Teman Netra", "Donasi Masker dan Gloves COVID-19"]
+    let date: [String] = ["20 Mei 2020", "30 Mei 2020", "15 Mei 2020", "15 Mei 2020", "1 Juni 2020", "30 Juni 2020"]
+    let location: [String] = ["DIY Yogyakarta", "DKI Jakarta", "Tangerang Selatan", "Jakarta Pusat", "Sumatera Selatan", "Bali"]
+    let tag: [UIImage] = [UIImage(named: "tag.png")!,UIImage(named: "tag.png")!,UIImage(named: "tag.png")!,UIImage(named: "tag.png")!,UIImage(named: "tag.png")!,UIImage(named: "tag.png")!,]
+    let due: [String] = ["3 days left", "5 days left", "10 Mei 2020", "8 Mei 2020", "20 Mei 2020", "1 Juni 2020"]
+    let logo: [UIImage] = [UIImage(named: "logo1.png")!,UIImage(named: "logo2.png")!,UIImage(named: "logo3.png")!,UIImage(named: "logo4.png")!,UIImage(named: "logo5.png")!,UIImage(named: "logo6.png")!,]
+    
+    //content in category collection view
+    let icon: [UIImage] = [UIImage(named: "category1.png")!,UIImage(named: "category2.png")!,UIImage(named: "category3.png")!,UIImage(named: "category4.png")!,UIImage(named: "category5.png")!,UIImage(named: "category6.png")!,UIImage(named: "category7.png")!,UIImage(named: "category8.png")!]
+    let category: [String] = ["Highlight", "COVID-19", "Children", "Education", "Disablity", "Disaster", "Donation", "On-Site"]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        setupNavBar()
+        view.backgroundColor = .white
+        setupNavBar()
+       //navigationController?.navigationBar.barStyle = .black
+       // navigationController?.navigationBar.isTranslucent = false
+        navigationItem.largeTitleDisplayMode = .always
+        edgesForExtendedLayout = []
+        navigationController?.navigationBar.isTranslucent = false
+
     }
+ 
+    
+    func setupNavBar(){
+        //large title for navbar
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        //navbar background color
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(red: 0, green: 0.711, blue: 0.867, alpha: 1)
+        
+        //change title color
+
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationItem.standardAppearance = appearance
+        navigationItem.scrollEdgeAppearance = appearance
+        navigationItem.compactAppearance = appearance
+      
+        //create search bar
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.delegate = self
+
+        //change search bar color
+        let searchBar = searchController.searchBar
+        searchBar.tintColor = UIColor.white
+        searchBar.barTintColor = UIColor.white
+        searchBar.isTranslucent = false
+        searchBar.isOpaque = true
+        
+        //change textfield for search bar color
+        if let textfield = searchBar.value(forKey: "searchField") as? UITextField {
+            textfield.textColor = UIColor.black
+            textfield.backgroundColor = UIColor.white
+            textfield.layer.cornerRadius = 10
+            textfield.clipsToBounds = true
+        }
+       
+    }
+    
+    // How many rows in the table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    //Defines what cells are being used
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "exploreCardCell", for: indexPath) as! ExploreCardCell
+        
+        cell.configure(picture: pictures[indexPath.row], date: date[indexPath.row], title: titles[indexPath.row], location: location[indexPath.row], tags: tag[indexPath.row], duedate: due[indexPath.row], logos: logo[indexPath.row])
+        
+        return cell
+}
+
+    //How many items in collection view
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+           return 8
+       }
+       
+    //Defines the content of the collection view
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+           let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "categoriesCell", for: indexPath) as! CategoryCell
+        
+            cell.categorySet(icons: icon[indexPath.row], categoryname: category[indexPath.row])
+           return cell
+       }
 }
--- Kurela/Controller/ProfileViewController.swift
+++ Kurela/Controller/ProfileViewController.swift
@@ -2,17 +2,104 @@
 //  ProfileViewController.swift
 //  Kurela
 //
-//  Created by M Habib Ali Akbar on 17/05/20.
+//  Created by Feby Lailani on 19/05/20.
 //  Copyright © 2020 Group 16 MC2 2020. All rights reserved.
 //
 
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    @IBOutlet weak var aboutButton: UIButton!
+    @IBOutlet weak var aboutLabel: UILabel!
+    @IBOutlet weak var medicalButton: UIButton!
+    @IBOutlet weak var medicalLabel: UILabel!
+    @IBOutlet weak var documentButton: UIButton!
+    @IBOutlet weak var documentLabel: UILabel!
+    @IBOutlet weak var emergencyButton: UIButton!
+    @IBOutlet weak var emergencyLabel: UILabel!
+    @IBOutlet weak var aboutView: UIView!
+    @IBOutlet weak var MedicalView: UIView!
+    @IBOutlet weak var documentView: UIView!
+    @IBOutlet weak var emergencyView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Do any additional setup after loading the view.
     }
+    
+    func prepareAboutView () {
+        aboutView.isHidden = false
+        MedicalView.isHidden = true
+        documentView.isHidden = true
+        emergencyView.isHidden = true
+    }
+    
+    func prepareMedicalView () {
+        aboutView.isHidden = true
+        MedicalView.isHidden = false
+        documentView.isHidden = true
+        emergencyView.isHidden = true
+    }
+    
+    func prepareDocumentView() {
+        aboutView.isHidden = true
+        MedicalView.isHidden = true
+        documentView.isHidden = false
+        emergencyView.isHidden = true
+    }
+    
+    func prepareEmergencyView() {
+        aboutView.isHidden = true
+        MedicalView.isHidden = true
+        documentView.isHidden = true
+        emergencyView.isHidden = false
+    }
+    
+    
+    
+    @IBAction func aboutTapped(_ sender: Any) {
+        prepareAboutView()
+        
+    }
+    
+    @IBAction func medicalTapped(_ sender: Any) {
+        prepareMedicalView()
+        
+    }
+    
+    @IBAction func documentTapped(_ sender: UIButton) {
+        prepareDocumentView()
+    }
+    
+    @IBAction func emergencyTapped(_ sender: UIButton) {
+        prepareEmergencyView()
+    }
+    
+    
+    
+    // @IBAction func selectSegment(_ sender: UISegmentedControl) {
+//        if sender.selectedSegmentIndex == 0 {
+//            currentSegmentedIndex = .idol
+//
+//        } else {
+//            currentSegmentedIndex = .cuisine
+//
+//        }
+//
+//        exploreCollectionView.reloadData()
+//    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+  
+    
     
 }
